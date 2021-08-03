8fc05d2075b58648ae1203313baa25765f844bff
orels1 <ping@orels.sh>
2021-08-03 13:00:58 +0300

    added Reload Last Build tool and some usage instructions

---
+++ Editor/ReloadLastBuild.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace orels1EScripts {
+  public class ReloadLastBuild : Editor {
+    [MenuItem("Tools/Reload Last Build")]
+    public static void RunReloadLastBuild() {
+      var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+      path = Path.Combine(path, @"..\LocalLow\VRChat\VRChat\Worlds");
+      try {
+        var lastWorldFile = new DirectoryInfo(path).GetFiles().OrderByDescending(o => o.LastWriteTime).FirstOrDefault();
+        if (lastWorldFile == null) {
+          Debug.Log("[ReloadLastBuild]: Failed to find world file. Have you built before?");
+          return;
+        }
+
+        File.SetLastWriteTime(lastWorldFile.FullName, DateTime.Now);
+        Debug.Log($"[ReloadLastBuild]: Reloaded {lastWorldFile.Name}");
+      }
+      catch (DirectoryNotFoundException e) {
+        Debug.Log("[ReloadLastBuild]: Failed to find world file. Have you built before?");
+        Debug.LogError(e);
+      }
+      catch (Exception e) {
+        Debug.Log("[ReloadLastBuild]: Encountered an error");
+        Debug.LogError(e);
+      }
+    }
+  }
+}
--- README.md
+++ README.md
@@ -17,6 +17,22 @@
 - Unpack anywhere in the Assets folder
 - Enjoy 
 
+## Tools
+
+- ImageAsPlane: allows simple creation of images on planes in front of your scene view camera
+    - Features
+        - Creates materials for your planes
+        - Shares existing materials when passing the same image
+        - Unlit/Static flags for faster setup
+    - Usage
+        - Right click in your hierarchy -> 3D Object -> Image As Plane
+        - Window / orels1 / Image As Plane
+- ReloadLastBuild: looks up the last built scene and makes the clients reload it (requires latest 2019 VRCSdk and [following the docs](https://docs.vrchat.com/v2021.3.2/docs/using-build-test#build--relaunch))
+    - Features
+        - Discovers the last built scene and Updates its modification time to force clients to relaunch the world
+    - Usage
+        - Tools / Reload Last Build
+
 ## Copyright
 
 Copyright (c) 2020 orels1
